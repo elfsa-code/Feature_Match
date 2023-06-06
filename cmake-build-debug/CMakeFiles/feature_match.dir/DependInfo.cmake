576d349e26072f74c8a4a39055da380cfcf3469e
elfsa-code <elfsaikou@gmail.com>
2023-06-06 13:25:17 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/feature_match.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/elfsa/Feature_Match-simple/source/feature_matcher.cpp" "/home/elfsa/Feature_Match-simple/cmake-build-debug/CMakeFiles/feature_match.dir/source/feature_matcher.cpp.o"
+  "/home/elfsa/Feature_Match-simple/source/vfc.cpp" "/home/elfsa/Feature_Match-simple/cmake-build-debug/CMakeFiles/feature_match.dir/source/vfc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
